9ee4ef54890768f1e2a623a77102a8feddcad6d8
Kraus-Mara <krausantoi@cy-tech.fr>
2025-12-10 14:31:21 +0100

    everforest colorscheme for the moment

--- config/core/colorscheme.nix
+++ config/core/colorscheme.nix
@@ -1,7 +1,6 @@
 {
   # colorscheme to match Mara of the Woods NixOS config
-  # light blue / light yellow / light pink / white / green #689d6a / background 
-  colorschemes.gruvbox = {
-    enable = false;
+  colorschemes.everforest = {
+    enable = true;
   };
 }
